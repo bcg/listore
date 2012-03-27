3b57f2958b0daa18491ff4099cf4265fb25f1e40
Brenden Grace <brenden.grace@gmail.com>
2012-03-27 10:00:58 -0500

    Updated the structures to be more memory efficient.

--- listore.cc
+++ listore.cc
@@ -1,18 +1,17 @@
+#include "stdio.h"
 #include <iostream>
 #include <map>
-#include <list>
+#include <vector>
 
 int main() {
 
-  std::map< std::string,std::list<int> > ilmap;
+  std::map< long int,std::vector<unsigned short> > ilmap;
   int i;
  
   for(i = 0; i < 10000000; i++) {
-    std::list<int> l;
-    char key[33];
-    sprintf(key, "%d", i);
-    l.assign(50,100);
-    ilmap.insert(std::make_pair(key,l));
+    std::vector<unsigned short> l;
+    l.assign(10,10000);
+    ilmap.insert(std::make_pair(i,l));
   }
   std::cout << "Done.\n";
   std::cin >> i;
